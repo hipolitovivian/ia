e0aba172c5790703646206ca23b2edc51e9c3917
hipolitovivian <125373072+hipolitovivian@users.noreply.github.com>
2024-04-15 20:22:21 -0300

    Add files via upload

---
+++ ia.py
@@ -0,0 +1,19 @@
+def knapsack(weights, values, max_weight):
+    n = len(weights)
+    dp = [[0] * (max_weight + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for w in range(1, max_weight + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+
+    selected_items = []
+    w = max_weight
+    for i in range(n, 0, -1):
+        if dp[i][w] != dp[i - 1][w]:
+            selected_items.append(i - 1)
+            w -= weights[i - 1]
+
+    return dp[n][max_weight], selected_items
